97af56f97f38b807be0723d14f915419b2c2d69f
Libra <97220040@qq.com>
2025-03-08 22:46:31 +0800

    resume and about page

---
+++ app/(fronted)/about/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Author: Libra
+ * Date: 2025-03-08 22:46:12
+ * LastEditors: Libra
+ * Description:
+ */
+"use client";
+export default function About() {
+  return <div>About Page</div>;
+}
---
+++ app/(fronted)/resume/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Author: Libra
+ * Date: 2025-03-08 22:45:45
+ * LastEditors: Libra
+ * Description:
+ */
+"use client";
+export default function Resume() {
+  return <div>Resume Page</div>;
+}
